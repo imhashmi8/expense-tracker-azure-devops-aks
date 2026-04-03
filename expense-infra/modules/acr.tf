68a872024d576cbc2bdd1005e364927984c597cc
imhashmi8 <qamar.hashmi@outlook.com>
2026-04-04 01:02:42 +0530

    updated acr name in tf and values.yaml file

--- expense-infra/modules/acr.tf
+++ expense-infra/modules/acr.tf
@@ -1,5 +1,5 @@
 resource "azurerm_container_registry" "main" {
-  name                = "${substr(lower(regexreplace(var.prefix, "[^a-zA-Z0-9]", "")), 0, 18)}${local.environment}${random_string.suffix.result}"
+  name                = substr(lower(regexreplace("${var.prefix}${var.environment}acr", "[^a-zA-Z0-9]", "")), 0, 50)
   resource_group_name = azurerm_resource_group.main.name
   location            = azurerm_resource_group.main.location
   sku                 = var.acr_sku
